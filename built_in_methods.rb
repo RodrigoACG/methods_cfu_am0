46b3ab46557738b2cba1dc5a0beeee80c920da3e
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-01-13 02:21:49 -0700

    Finish built in methods

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -55,10 +55,25 @@ puts last_name.downcase
 # # https://ruby-doc.org/core-3.1.0/Integer.html
 # # Include comments above each method call explaining the impact and return value of that method.
 
+puts [2,3].sum
+# In this method it is adding up the 2 numbers. It can also do do a lot of numbers not just 2.
 
+p 8.integer?
+# in this method it is asking if it is an integer.
 
 # # SECTION 4: Calling methods on variables assigned to arrays.
 # # Declare 2 variables assigned to arrays.
 # # Call a different built-in Ruby method on each of your variables. 
 # # https://ruby-doc.org/core-3.1.0/Array.html
 # # Include comments above each method call explaining the impact and return value of that method.
+
+p friends = [ "Tyara", "Rodrigo", "Erik", "Martin", "Ezequiel", "Jason", "Jorge"]
+p age = [20,19,18,21,19,18,18]
+p friends.length
+# In this method it is showing the amounbt of friends in the array.
+p friends.pop
+# In this method it is taking one of friends out of the array.
+p friends.clear 
+# In this method it is clearing all the names out of the array, so nothing is left in the array.
+p age.uniq
+# In this method it is taking the ones that have been said already. For example, 18 was said 3 times and the output has only 1. 
